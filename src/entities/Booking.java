92932d325462b1b86156489e94ed6ec9599d1b55
Alam zaib <afridialamzaib900@gmail.com>
2026-02-06 00:51:06 +0600

    Modify entity class

--- src/entities/Booking.java
+++ src/entities/Booking.java
@@ -1,5 +1,22 @@
 package entities;
 
-public class Booking {
+import java.time.LocalDateTime;
 
+public class Booking {
+    private int memberId;
+    private String classTitle;
+    private int classInstructorId;
+    private LocalDateTime bookingDate;
+
+    public Booking(int memberId, String classTitle, int classInstructorId, LocalDateTime bookingDate) {
+        this.memberId = memberId;
+        this.classTitle = classTitle;
+        this.classInstructorId = classInstructorId;
+        this.bookingDate = bookingDate;
+    }
+
+    public int getMemberId() { return memberId; }
+    public String getClassTitle() { return classTitle; }
+    public int getClassInstructorId() { return classInstructorId; }
+    public LocalDateTime getBookingDate() { return bookingDate; }
 }
